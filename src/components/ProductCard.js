d56d5d0a89b57d4bca6643d6e4c9d01da7f1748d
Exxar717 <121441127+Exxar717@users.noreply.github.com>
2023-06-21 17:06:28 +0600

    fixed popular product

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -16,7 +16,13 @@ const ProductCard = (props) => {
   return (
     <>
         <div className={` ${location.pathname == "/product" ? `gr-${grid}` : "col-3"}`}>
-        <Link to=':id' className="product-card position-relative">
+        <Link to={`${
+            location.pathname=='/'
+            ? '/product/:id'
+            : location.pathname == '/product/:id'
+            ? '/product/:id'
+            : ':id'}`} 
+            className="product-card position-relative">
             <div className="wishlist-icon position-absolute">
                 <button className='border-0 bg-transparent'><img src={wish} alt="wishlist" /></button>
             </div>
@@ -49,7 +55,14 @@ const ProductCard = (props) => {
         </Link>
     </div>
     <div className={` ${location.pathname == "/product" ? `gr-${grid}` : "col-3"}`}>
-        <Link className="product-card position-relative">
+        <Link
+          to={`${
+            location.pathname=='/'
+            ? '/product/:id'
+            : location.pathname == '/product/:id'
+            ? '/product/:id'
+            : ':id'}`}
+            className="product-card position-relative">
             <div className="wishlist-icon position-absolute">
                 <button className='border-0 bg-transparent'><img src={wish} alt="wishlist" /></button>
             </div>
